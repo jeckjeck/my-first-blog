eea672b0777517a5ce020aedf7beb7bde71d2365
Joakim Bäcklund <joakim.backlund85@gmail.com>
2017-02-11 20:53:42 +0100

    Fixing 3Dplot without link

--- polls/views.py
+++ polls/views.py
@@ -45,8 +45,6 @@ def simple(request):
     import numpy as np
     from mpl_toolkits.mplot3d import Axes3D
     import matplotlib.pyplot as plt
-    from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg \
-        as FigureCanvas
     import random
 
     def fun(x, y):
